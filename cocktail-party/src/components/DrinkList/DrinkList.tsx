8d621fc10a338bed58bdc085f887280e30f067dc
Ivan Dzubansky <i.dzubansky@gmail.com>
2023-05-23 10:16:04 +0200

    changed layout of drink cards

--- cocktail-party/src/components/DrinkList/DrinkList.tsx
+++ cocktail-party/src/components/DrinkList/DrinkList.tsx
@@ -11,21 +11,25 @@ function DrinkList({ drinks }: Props) {
     <motion.div className="m-auto flex flex-col items-center gap-4">
       {drinks.map((drink: Drink, index: number) => (
         <motion.div
+          className="w-11/12"
           key={drink.id}
           transition={{ delay: 0.1 * index, duration: 0.3 }}
           initial={{ opacity: 0, x: -200 }}
           animate={{ opacity: 1, x: 0 }}
           exit={{ opacity: 0, x: 200 }}
         >
-          <Link className="w-full" scroll={false} href={`/drink/${drink.id}`}>
-            <div className="m-auto flex w-11/12 flex-col items-center rounded-3xl bg-white text-center shadow-lg">
-              <div className="w-full rounded-3xl bg-white shadow-lg">
+          <Link scroll={false} key={drink.id} href={`/drink/${drink.id}`}>
+            <div className="flex h-28 w-full content-start items-center rounded-3xl bg-white text-center shadow-xl">
+              <div className="flex h-full w-28 items-center justify-center rounded-3xl bg-white shadow-lg">
                 <img
+                  className="h-full w-full rounded-3xl object-cover shadow"
                   src={drink.imageUrl}
                   alt={drink.name}
-                  className="h-full w-full rounded-3xl object-cover"
                 />
               </div>
+              <h3 className="flex w-2/3 justify-center bg-gradient-to-r from-indigo-700 via-purple-700 to-pink-700 bg-clip-text text-xl font-extrabold text-transparent">
+                {drink.name}
+              </h3>
             </div>
           </Link>
         </motion.div>
